57c5f260fa07ba019232832f7a2f29d9daae0b04
igorpex <igor.bogdanov@gmail.com>
2022-07-03 22:52:27 +0300

    refactor: add BUCKET to environment

--- import-service-ts/serverless.ts
+++ import-service-ts/serverless.ts
@@ -33,6 +33,7 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      BUCKET,
     },
   },
   // import the function via paths
