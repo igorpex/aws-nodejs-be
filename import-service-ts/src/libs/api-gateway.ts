2831f1b39d8521338989c1f0d1f071e0c909d396
igorpex <igor.bogdanov@gmail.com>
2022-07-03 20:38:30 +0300

    refactor: move headers to api-gateways lib

--- import-service-ts/src/libs/api-gateway.ts
+++ import-service-ts/src/libs/api-gateway.ts
@@ -4,8 +4,15 @@ import type { FromSchema } from "json-schema-to-ts";
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Credentials": true,
+  "Content-Type": "application/json;charset=UTF-8",
+}
+
 export const formatJSONResponse = (response: Record<string, unknown>) => {
   return {
+    headers: headers,
     statusCode: 200,
     body: JSON.stringify(response)
   }
