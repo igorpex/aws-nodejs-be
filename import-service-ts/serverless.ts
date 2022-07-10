97c52bd17e37363b25b6b210307112b9ba0775fe
igorpex <igor.bogdanov@gmail.com>
2022-07-10 13:48:10 +0300

    feat: add SQS, update importFileParset to work with SQS

--- import-service-ts/serverless.ts
+++ import-service-ts/serverless.ts
@@ -1,6 +1,7 @@
 import type { AWS } from '@serverless/typescript';
 import 'dotenv/config';
 const BUCKET = process.env.BUCKET;
+// const SQS_URL = process.env.SQS_URL;
 
 import importFileParser from '@functions/importFileParser';
 import importProductsFile from '@functions/importProductsFile';
@@ -24,6 +25,11 @@ const serverlessConfiguration: AWS = {
         Effect: 'Allow',
         Action: 's3:*',
         Resource: `arn:aws:s3:::${BUCKET}/*`
+      },
+      {
+        Effect: 'Allow',
+        Action: 'sqs:*',
+        Resource: { 'Fn::GetAtt': ['SQSQueue', 'Arn'] }
       }
     ],
     apiGateway: {
@@ -34,10 +40,38 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
       BUCKET,
+      SQS_URL: { 'Fn::GetAtt': ['SQSQueue', 'QueueUrl'] },
+      SQS_NAME: { 'Fn::GetAtt': ['SQSQueue', 'QueueName'] },
+      SQS_ARN: { 'Fn::GetAtt': ['SQSQueue', 'Arn'] },
     },
   },
   // import the function via paths
   functions: { importProductsFile, importFileParser },
+  resources: {
+    Resources: {
+      SQSQueue: {
+        Type: 'AWS::SQS::Queue',
+        Properties: {
+          QueueName: 'import-service-ts-queue',
+        }
+      },
+      SNSTopic: {
+        Type: 'AWS::SNS::Topic',
+        Properties: {
+          TopicName: 'createProductTopic',
+        }
+      },
+      SNSSubscription: {
+        Type: 'AWS::SNS::Subscription',
+        Properties: {
+          Endpoint: 'igor.bogdanov@gmail.com',
+          Protocol: 'email',
+          TopicArn: { 'Fn::GetAtt': ['SNSTopic', 'Arn'] },
+        }
+      }
+    }
+  },
+
   package: { individually: true },
   custom: {
     esbuild: {
--- import-service-ts/src/functions/importFileParser/handler.ts
+++ import-service-ts/src/functions/importFileParser/handler.ts
@@ -1,7 +1,7 @@
 import { middyfy } from '@libs/lambda';
 import { Handler, S3Event } from 'aws-lambda';
 import { InvokeAsyncResponse } from 'aws-sdk/clients/lambda';
-import * as AWS from 'aws-sdk';
+import { S3, SQS } from 'aws-sdk';
 import csv from 'csv-parser';
 
 import 'dotenv/config';
@@ -10,7 +10,9 @@ const importFileParser: Handler = async (event: S3Event): Promise<InvokeAsyncRes
   console.log('New object appeared in uploaded folder. Event.Records:', event.Records);
   try {
     const BUCKET = process.env.BUCKET;
-    const s3 = new AWS.S3({ region: 'eu-west-1' });
+    const SQS_URL = process.env.SQS_URL;
+    const SQS_NAME = process.env.SQS_NAME;
+    const s3 = new S3({ region: 'eu-west-1' });
     console.log("Bucket:", BUCKET);
     const record = event.Records[0];
     const s3Stream = s3
@@ -20,11 +22,49 @@ const importFileParser: Handler = async (event: S3Event): Promise<InvokeAsyncRes
       })
       .createReadStream();
 
+    const sqs = new SQS();
+    console.log('####SQS_URL:', SQS_URL);
+
+    // const sendToQueue = async (message: string) => {
+    //   const SQS_Params = {
+    //     MessageBody: message,
+    //     QueueUrl: SQS_URL,
+    //   };
+    //   try {
+    //     const result = await sqs.sendMessage(SQS_Params).promise();
+    //     console.log("SQS Message send successfully....", result);
+    //     return result;
+    //   } catch (error) {
+    //     console.error("Error in SQS send message: ", error.stack);
+    //     return error;
+    //   }
+    // };
+
+    // sqs.getQueueUrl({ QueueName: SQS_URL }, (res) => console.log('received URL:', res));
+
+    var params = {
+      QueueName: SQS_NAME
+    };
+    // sqs.getQueueUrl(params, function (err, data) {
+    //   if (err) console.log(err, err.stack); // an error occurred
+    //   else console.log('received URL:', data);           // successful response
+    // });
+
+    // Get QueueUrl (alternative way, as 'QueueUrl' in ref  "SQS_URL: { 'Fn::GetAtt': ['SQSQueue', 'QueueUrl'] }" officially unsupported by AWS" )
+    let sqsUrlData = await sqs.getQueueUrl(params).promise();
+    let SQS_URL1 = sqsUrlData.QueueUrl;
+    console.log('SQS_URL1', SQS_URL1);
+
     await new Promise((resolve, reject) => {
       s3Stream
         .pipe(csv())
         .on('data', (data) => {
           console.log(data);
+          // sendToQueue(JSON.stringify(data));
+          sqs.sendMessage({
+            MessageBody: JSON.stringify(data),
+            QueueUrl: SQS_URL,
+          }, () => console.log("SQS Message send successfully...."))
         })
         .on('error', (error) => reject('ERROR: ' + error))
         .on('end', async () => {
@@ -41,7 +81,7 @@ const importFileParser: Handler = async (event: S3Event): Promise<InvokeAsyncRes
             })
             .promise();
 
-          console.log(`Deleting from ${BUCKET}/${srcObj} to ${BUCKET}/${destObj}...`);
+          console.log(`Deleting from ${BUCKET}/${srcObj}...`);
           await s3
             .deleteObject({
               Bucket: BUCKET,
